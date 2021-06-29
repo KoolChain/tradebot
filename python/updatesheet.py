2a5a56c49cebdb747f60f12647a706d4d4f7a3a6
Adrien <Adnn@users.noreply.github.com>
2021-06-29 19:09:46 +0200

    Export from updatesheet.py: balance datetime and order quote amount.

--- python/updatesheet.py
+++ python/updatesheet.py
@@ -21,7 +21,8 @@ def insert_order_formulas(orders):
             + order[1:] \
             + ('=DSUM(Fragments!$A:$I; "taken_home"; {Fragments!$I$1;INDIRECT(CONCAT("A"; ROW()))})',) \
             + ('=IF(H:H=0; P:P/F:F; P:P/(F:F*L:L))',)   \
-            + ('=IF(H:H=0; F:F*(G:G-L:L); "")',)
+            + ('=IF(H:H=0; F:F*(G:G-L:L); "")',)        \
+            + ('=F:F*L:L',)
 
 
 def append_orders(sheet_api, database):
@@ -46,17 +47,22 @@ def append_fragments(sheet_api, database, last_order):
     sheet_api.append("Fragments", database.get_fragments(max_order_id, last_order))
 
 
-def append_balances(sheet_api, database):
-    previous_time = last_integer(sheet_api, "Balances!B:B", 0)
-    print("Previous balance time {}.".format(previous_time))
-    balances = database.get_balances_after_time(previous_time)
-    values = []
+def insert_balance_formulas(balances, previous_time, database):
     for balance in balances:
         time = balance[1]
         count = database.count_launch_period(previous_time, time)
-        values.append(balance + (count,))
         previous_time = time
-    sheet_api.append("Balances", values)
+        yield balance[:1]   \
+            + ('=from_epoch(INDIRECT("C" & ROW()))',) \
+            + balance[1:]   \
+            + (count,)
+
+
+def append_balances(sheet_api, database):
+    previous_time = last_integer(sheet_api, "Balances!C:C", 0)
+    print("Previous balance time {}.".format(previous_time))
+    balances = database.get_balances_after_time(previous_time)
+    sheet_api.append("Balances", list(insert_balance_formulas(balances, previous_time, database)))
 
 
 def main():
